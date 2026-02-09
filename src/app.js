85a1ce45dc91eb2793c7c48bc873ff22a8d45f1d
Prathmesh6968 <prathmeshcoder69@gmail.com>
2026-02-10 01:18:24 +0530

    Add search router to app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import router from "./routes/fresh-drop.routes.js";
 import episoderouter from "./routes/episode.routes.js";
 import iframeRouter from "./routes/iframe.routes.js";
+import searchRouter from "./routes/search.routes.js";
 
 const app = express();
 
 app.use("/api/hindi/recent",router);
 app.use("/api/hindi/episode",episoderouter)
 app.use("/api/hindi/stream/",iframeRouter)
+app.use("/api/hindi/search",searchRouter)
 
 export default app;
